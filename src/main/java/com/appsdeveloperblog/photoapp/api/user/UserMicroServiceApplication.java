498aa969c57829394bfaddbe1b11d3c4edb64da3
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-08-19 19:54:05 +0200

    first commit

---
+++ src/main/java/com/appsdeveloperblog/photoapp/api/user/UserMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.api.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMicroServiceApplication.class, args);
+    }
+
+}
